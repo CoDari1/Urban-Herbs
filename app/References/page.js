b4f860a227f024f224fccd2f1252c4de2367e576
Darius McQueen <darmcq8632@ecsk12.org>
2025-04-14 23:39:12 +0000

    final

--- app/References/page.js
+++ app/References/page.js
@@ -3,28 +3,187 @@
 import React from 'react';
 
 const Page = () => {
-    const openWorklog = () => {
-        window.open('/pdfs/Worklog.pdf', '_blank', 'noopener,noreferrer');
-    };
-
     return (
-        <div className={'mt-52'}>
+        <div className="mt-20 space-y-4">
+            <section>
+                <h2 className="text-2xl font-semibold text-indigo-600 mb-4">Links</h2>
+                <ul className="space-y-2">
+                    <li className="text-lg">
+                        <a
+                            href="https://www.number-2-pencil.com/easy-buttery-garlic-parmesan-breadsticks/"
+                            target="_blank"
+                            className="text-blue-500 hover:underline"
+                        >
+                            1. Easy Buttery Garlic Parmesan Breadsticks
+                        </a>
+                    </li>
+
+                    <li className="text-lg">
+                        <a
+                            href="https://www.food.com/recipe/ranch-deviled-eggs-477999"
+                            target="_blank"
+                            className="text-blue-500 hover:underline"
+                        >
+                            2. Ranch Deviled Eggs
+                        </a>
+                    </li>
+
+                    <li className="text-lg">
+                        <a
+                            href="https://www.theedgyveg.com/2021/10/25/vegan-mozzarella-sticks/"
+                            target="_blank"
+                            className="text-blue-500 hover:underline"
+                        >
+                            3. Vegan Mozzarella Sticks
+                        </a>
+                    </li>
+
+                    <li className="text-lg">
+                        <a
+                            href="https://www.epicurious.com/recipes/food/views/smoked-feta-dip"
+                            target="_blank"
+                            className="text-blue-500 hover:underline"
+                        >
+                            4. Smoked Feta Dip
+                        </a>
+                    </li>
+
+                    <li className="text-lg">
+                        <a
+                            href="https://www.bonappetit.com/recipe/hurricane-chips"
+                            target="_blank"
+                            className="text-blue-500 hover:underline"
+                        >
+                            5. Hurricane Chips
+                        </a>
+                    </li>
+
+                    <li className="text-lg">
+                        <a
+                            href="https://thecozycook.com/broccoli-cheese-soup/"
+                            target="_blank"
+                            className="text-blue-500 hover:underline"
+                        >
+                            6. Broccoli Cheese Soup
+                        </a>
+                    </li>
+
+                    <li className="text-lg">
+                        <a
+                            href="https://www.eatingwell.com/recipe/7994048/smashed-zucchini-with-pesto-burrata/"
+                            target="_blank"
+                            className="text-blue-500 hover:underline"
+                        >
+                            7. Smashed Zucchini with Pesto Burrata
+                        </a>
+                    </li>
+
+                    <li className="text-lg">
+                        <a
+                            href="https://www.loveandlemons.com/grilled-vegetables/"
+                            target="_blank"
+                            className="text-blue-500 hover:underline"
+                        >
+                            8. Grilled Vegetables
+                        </a>
+                    </li>
+
+                    <li className="text-lg">
+                        <a
+                            href="https://www.eatingbirdfood.com/classic-minestrone-soup/"
+                            target="_blank"
+                            className="text-blue-500 hover:underline"
+                        >
+                            9. Classic Minestrone Soup
+                        </a>
+                    </li>
+
+                    <li className="text-lg">
+                        <a
+                            href="https://cooking.nytimes.com/recipes/1014527-basic-polenta"
+                            target="_blank"
+                            className="text-blue-500 hover:underline"
+                        >
+                            10. Basic Polenta
+                        </a>
+                    </li>
+
+                    <li className="text-lg">
+                        <a
+                            href="https://www.loveandlemons.com/margherita-pizza/"
+                            target="_blank"
+                            className="text-blue-500 hover:underline"
+                        >
+                            11. Veggie Pizza
+                        </a>
+                    </li>
+
+                    <li className="text-lg">
+                        <a
+                            href="https://smittenkitchen.com/2023/09/simple-eggplant-parmesan/"
+                            target="_blank"
+                            className="text-blue-500 hover:underline"
+                        >
+                            12. Eggplant Parmesan
+                        </a>
+                    </li>
+
+                    <li className="text-lg">
+                        <a
+                            href="https://www.thereciperebel.com/one-pot-pasta-primavera/"
+                            target="_blank"
+                            className="text-blue-500 hover:underline"
+                        >
+                            13. One Pot Pasta
+                        </a>
+                    </li>
+
+                    <li className="text-lg">
+                        <a
+                            href="https://www.thepioneerwoman.com/food-cooking/recipes/a10208/caprese-salad/"
+                            target="_blank"
+                            className="text-blue-500 hover:underline"
+                        >
+                            14. Caprese Salad
+                        </a>
+                    </li>
+
+                    <li className="text-lg">
+                        <a
+                            href="https://www.recipetineats.com/penne-all-arrabbiata-spicy-tomato-pasta/"
+                            target="_blank"
+                            className="text-blue-500 hover:underline"
+                        >
+                            15. Arrabbiata
+                        </a>
+                    </li>
+                </ul>
+            </section>
+
             <div>
-                <h2>
-                    Image Sources
-                </h2>
+                Copyright Checklist: 
+                <a 
+                    href="https://onedrive.live.com/?cid=81690735673AC524&id=81690735673AC524%21sc71eca4f085f46d6b6e9b23343efb83f&parId=root&o=OneUp" 
+                    target="_blank"
+                    className="text-blue-500 underline"
+                >
+                    Here
+                </a>
             </div>
 
             <div>
-                Copyright Checklist: <a href={''}>Here</a>
+                Worklog: 
+                <a 
+                    href="https://1drv.ms/w/c/81690735673ac524/EQi4Ux9GwJNBrdLPnG6WL_kBWe3Q7cbAyUz7vJXXFuk1rg?e=fNQuB3" 
+                    target="_blank"
+                    className="text-blue-500 underline ml-1"
+                >
+                    Here
+                </a>
             </div>
 
             <div>
-                Worklog: <button onClick={openWorklog} className="text-blue-500 underline">Here</button>
-            </div>
-
-            <div>
-                Technologies used: Next.JS, TailwindCSS, ShadCN
+                Technologies used: Next.js, TailwindCSS, ShadCN, and Vercel
             </div>
         </div>
     );
